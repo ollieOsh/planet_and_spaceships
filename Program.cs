d66dfcd4353064f0fe79deac8aecf0ac2d57c626
ollieOsh <sosinusi@gmail.com>
2017-07-10 14:20:59 -0500

    done

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            List<string> planetList2 = new List<string>(){"Uranus", "Neptune"};
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            planetList.AddRange(planetList2);
+            planetList.Insert(1,"Venus");
+            planetList.Insert(2,"Earth");
+            planetList.Add("Pluto");
+            
+            List<string> rockyPlanets = new List<string>(planetList.GetRange(0,4));
+
+            planetList.Remove("Pluto");
+
+            foreach (string planet in planetList) {
+                Console.WriteLine(planet);
+            }
+
+            foreach (string planet in rockyPlanets) {
+                Console.WriteLine(planet);
+            }
+        }
+    }
+}
